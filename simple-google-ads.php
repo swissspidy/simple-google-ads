9d9dab3df7273f9845ca9f1480c48b968dfa2c2e
Pascal Birchler <hello@pascalbirchler.ch>
2018-10-20 00:21:49 +0200

    Define domain path and generate pot file

--- simple-google-ads.php
+++ simple-google-ads.php
@@ -9,6 +9,7 @@
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: simple-google-ads
+ * Domain Path: /languages
  *
  * @package SimpleGoogleAds
  */
